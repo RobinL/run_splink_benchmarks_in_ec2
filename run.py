1462227ff6ddd2d23d4448bdbb9a7c470e076c9b
Robin Linacre <robin.linacre@digital.justice.gov.uk>
2023-12-14 21:17:47 +0000

    Refactor CloudWatch logging and S3 client initialization

--- run.py
+++ run.py
@@ -5,14 +5,16 @@ import boto3
 from watchtower import CloudWatchLogHandler
 
 
-def setup_cloudwatch_logging():
+def setup_cloudwatch_logging(region_name):
     # Create a logger
     logger = logging.getLogger(__name__)
     logger.setLevel(logging.INFO)
 
-    # Create a CloudWatch log handler
+    # Create a CloudWatch log handler with the specified region
     cw_handler = CloudWatchLogHandler(
-        log_group="MyTestLogGroup", stream_name="MyTestLogStream"
+        log_group="MyTestLogGroup",
+        stream_name="MyTestLogStream",
+        boto3_session=boto3.Session(region_name=region_name),
     )
 
     # Add the CloudWatch log handler to the logger
@@ -21,13 +23,13 @@ def setup_cloudwatch_logging():
     return logger
 
 
-def create_and_upload_file(bucket_name, file_name, content, logger):
+def create_and_upload_file(bucket_name, file_name, content, logger, region_name):
     # Create a file with the specified content
     with open(file_name, "w") as file:
         file.write(content)
 
-    # Initialize the S3 client
-    s3_client = boto3.client("s3")
+    # Initialize the S3 client with the specified region
+    s3_client = boto3.client("s3", region_name=region_name)
 
     # Upload the file to S3
     s3_client.upload_file(file_name, bucket_name, file_name)
@@ -35,15 +37,16 @@ def create_and_upload_file(bucket_name, file_name, content, logger):
 
 
 if __name__ == "__main__":
+    region_name = "eu-west-2"
     bucket = "robinsplinkbenchmarks"
     current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     file_name = f"hello_world_{current_time}.txt"
     content = "hi"
 
     # Setup CloudWatch logging
-    logger = setup_cloudwatch_logging()
+    logger = setup_cloudwatch_logging(region_name)
 
     # Log a message to CloudWatch
     logger.info("Hello World")
 
-    create_and_upload_file(bucket, file_name, content, logger)
+    create_and_upload_file(bucket, file_name, content, logger, region_name)
